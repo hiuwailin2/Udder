7ec59fda9ee0639c077f89282aacac24bd0e8a3f
John Lin <hiuwailin2@ymail.com>
2018-01-24 00:46:27 +0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by HIU WAI LIN on 24/1/2018.
+//  Copyright © 2018年 HIU WAI LIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
